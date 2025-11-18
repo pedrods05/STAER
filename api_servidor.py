0b81cbcf7b64dbb3db79e9220d56e9956728ea51
pedrods05 <1230773@isep.ipp.pt>
2025-11-18 10:14:42 +0000

    webapp start

--- api_servidor.py
+++ api_servidor.py
@@ -1,53 +1,37 @@
 import sqlite3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
-# O nome da base de dados que o 'fase1.py' está a preencher
 DB_FILE = "trafego_aereo.db"
 
-# Cria a aplicação web Flask
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='templates')
 def get_db_connection():
-    """Cria uma ligação à base de dados"""
     conn = sqlite3.connect(DB_FILE)
-    # Isto faz com que a base de dados devolva dicts em vez de tuplos
-    # o que é perfeito para converter para JSON!
+   
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
 def index():
-    return "Servidor API do Tráfego Aéreo. Tente /api/aeronaves"
+   return render_template('index.html')
 
 @app.route('/api/aeronaves')
 def get_aeronaves():
-    """
-    O endpoint principal da API.
-    Vai à base de dados e devolve todos os aviões como JSON.
-    """
+   
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        # Vai buscar todos os registos da tabela
         cursor.execute("SELECT * FROM aeronaves")
 
-        # Recolhe todos os resultados
-        # A 'row_factory' trata de os converter para uma lista de dicts
         lista_avioes = [dict(row) for row in cursor.fetchall()]
 
         conn.close()
 
-        # 'jsonify' é uma função do Flask que formata
-        # corretamente a nossa lista para JSON.
+   
         return jsonify(lista_avioes)
 
     except Exception as e:
-        # Devolve um erro 500 se algo correr mal
         return jsonify({"erro": str(e)}), 500
 
-# --- Bloco para correr o servidor ---
 if __name__ == '__main__':
-    # Corre o servidor. 
-    # O 'debug=True' é útil para desenvolvimento.
-    app.run(debug=True, port=5000)
+  app.run(host='0.0.0.0', debug=True, port=5000)
